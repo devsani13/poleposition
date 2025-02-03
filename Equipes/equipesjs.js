8151a1de3fcbd4b3781a0979c4953dc12d36343b
Sani <samuelmartinspassos@gmail.com>
2025-02-03 02:10:08 -0300

    Commit 030225

---
+++ Equipes/equipesjs.js
@@ -0,0 +1,29 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Controles seguinte / anterior
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Controles de imagem em miniatura
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length)
+  {
+    slideIndex = 1;
+  }
+  if (n < 1)
+  {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
